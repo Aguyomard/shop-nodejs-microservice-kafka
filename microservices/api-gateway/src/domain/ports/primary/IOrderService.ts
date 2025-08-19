a183a8893790bfeabb6a52a382c0f180a49e2c64
alec <alec.guyomard@calmedica.com>
2025-08-19 16:25:29 +0200

    archi gateway update 3

--- microservices/api-gateway/src/domain/ports/primary/IOrderService.ts
+++
@@ -1,5 +0,0 @@
-import { Cart, CartItem, OrderResponse } from '../index';
-
-export interface IOrderService {
-  createOrder(cart: Cart | CartItem[], userId: string): Promise<OrderResponse>;
-} 
--- microservices/api-gateway/src/domain/ports/primary/index.ts
+++ microservices/api-gateway/src/domain/ports/primary/index.ts
@@ -1,3 +1,2 @@
 // Ports Primaires (Inbound) - Le domaine expose ces interfaces
-export * from './IOrderService';
 export * from './ICreateOrderSaga'; 
